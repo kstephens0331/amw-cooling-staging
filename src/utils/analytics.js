34cacd24c19c17f3288be4c1601116edd681f41e
kstephens0331 <Kyle_stephens31@icloud.com>
2025-09-07 23:41:52 -0500

    upated

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -16,3 +16,11 @@ export const event = (action, params = {}) => {
     console.debug('event', action, params);
   }
 };
+export const initGA = () => {
+  if (!window.gtag) return;
+  window.gtag('config', 'G-PHMH5V5651', {
+    cookie_domain: 'amwaircondition.com',
+    cookie_flags: 'SameSite=Lax;Secure',
+    // GA4 anonymize_ip is on by default; no need to set
+  });
+};
